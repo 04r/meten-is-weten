366b30e850459339f22ee090eb8f2b4927a8bbba
Seyit a <seyitaliaydin04@gmail.com>
2021-09-08 10:46:42 +0200

    else statement

--- aa.py
+++ aa.py
@@ -1,5 +1,6 @@
-maxim = []
-minim = []
+maxim = ""
+minim = ""
+equal = ""
 
 getal1 = input("Getal 1: ")
 getal2 = input("Getal 2: ")
@@ -10,6 +11,13 @@ if getal1 > getal2:
 elif getal2 > getal1:
     maxim = f"{getal2}, getal 2"
     minim = f"{getal1}, getal 1"
+if getal1 == getal2:
+    equal = "Yes"
+    maxim = "Equal!"
+    minim = "Equal!"
+else:
+    equal = "No"
 
 print(f"Max: {maxim}")
 print(f"Min: {minim}")
+print(f"Equal? {equal}")
