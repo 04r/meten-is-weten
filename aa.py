c18a22eb2b1f8284f771a93bba118ac0fa60f7e8
Seyit a <seyitaliaydin04@gmail.com>
2021-09-08 10:20:32 +0200

    input, if en elif statement

--- aa.py
+++ aa.py
@@ -7,7 +7,7 @@ getal2 = input("Getal 2: ")
 if getal1 > getal2:
     minim = f"{getal1}, getal 1"
     maxim = f"{getal2}, getal 2"
-else:
+elif getal2 > getal1:
     maxim = f"{getal2}, getal 2"
     minim = f"{getal1}, getal 1"
 
