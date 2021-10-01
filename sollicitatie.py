a65a5436baee6ad2fcca788feb82a6ff63829571
04r <84754110+04r@users.noreply.github.com>
2021-10-01 09:24:00 +0200

    Update sollicitatie.py

--- sollicitatie.py
+++ sollicitatie.py
@@ -1,21 +1,74 @@
-naam = input("Wat is jouw naam: ")
-leeftijd = input("Hou oud ben je: ")
 
-prak = input("Hoeveel jaar praktijkervaring heeft u met dieren-dressuur? ")
+# Meer dan 4 jaar praktijkervaring met dieren-dressuur OF meer dan 5 jaar ervaring met jongleren OF meer dan 3 jaar praktijkervaring met acrobatiek
+# In bezit van een Diploma MBO-4 ondernemen
+# In bezit van een geldig Vrachtwagen rijbewijs
+# In bezit van een hoge hoed
+# Is man EN heeft Snor breder dan 10 cm OF is vrouw EN draagt rood krulhaar langer dan 20 cm.
+# Let op: Je hoeft alleen een man naar zijn snorlengte te vragen en alleen een vrouw naar haar krulhaarlengte.
+# Is langer dan 150 cm
+# Is zwaarder dan 90 kg
+# Heeft Certificaat “Overleven met gevaarlijk personeel”
 
 
-x1 = "In bezit van een Diploma MBO-4 ondernemen"
-x2 = "In bezit van een geldig Vrachtwagen rijbewijs "
-x3 = "In bezit van een hoge hoed "
-x4 = "Is man EN heeft Snor breder dan 10 cm OF is vrouw EN draagt rood krulhaar langer dan 20 cm "
-x5 = "Is langer dan 150 cm "
-x6 = "Gewicht "
-x7 = "Heeft Certificaat “Overleven met gevaarlijk personeel” "
 
-x11 = input(x1)
-x22 = input(x2)
-x33 = input(x3)
-x44 = input(x4)
-x55 = input(x5)
-x66 = input(x6)
-x77 = input(x7)
+# naam = input("Wat is jouw naam: ")
+# leeftijd = input("Hou oud ben je: ")
+
+# prak = input("Hoeveel jaar praktijkervaring heeft u met dieren-dressuur? ")
+
+
+diploma = "Heeft u meer dan 4 jaar praktijkervaring met dieren-dressuur OF meer dan 5 jaar ervaring met jongleren OF meer dan 3 jaar praktijkervaring met acrobatiek: "
+rijbewijs = "Geldig Vrachtwagen rijbewijs?: "
+hogehoed = "In bezit van een hoge hoed?: "
+morf = "Bent u een vrouw of een man?"
+vrouw = "Heeft u rood krulhaar langer dan 20 cm?: "
+man = "Heeft u een Snor breder dan 10 cm?: "
+cm = "Is langer dan 150 cm "
+gewicht = "Gewicht?: "
+certi = "Heeft Certificaat “Overleven met gevaarlijk personeel”?: "
+
+def main():
+
+    step = input("Bent u een man of een vrouw?: ").lower()
+
+    if step == "man":
+        x = input(diploma).lower()
+        if x == "ja":
+            xd = input(rijbewijs).lower()
+            if xd == "ja":
+                lel = input(hogehoed).lower()
+                if lel == "ja":
+                    aaa = input(cm).lower()
+                    if aaa == "ja":
+                        asdsad = input(man).lower()
+                        if asdsad == "ja":
+                            kilo = input("Bent u zwaarder dan 90 KG? ").lower()
+                            if kilo == "nee":
+                                print("Sorry, je moet boven de 90 KG zijn.")
+                            else:
+                                kgk = input(certi).lower()
+                                if kgk == "ja":
+                                    print("We nemen jou aan!")
+                                    exit()
+    elif step == "vrouw":
+        x = input(diploma).lower()
+        if x == "ja":
+            xd = input(rijbewijs).lower()
+            if xd == "ja":
+                lel = input(hogehoed).lower()
+                if lel == "ja":
+                    aaa = input(cm).lower()
+                    if aaa == "ja":
+                        asdsad = input(vrouw).lower()
+                        if asdsad == "ja":
+                            kilo = input("Bent u zwaarder dan 90 KG?").lower()
+                            if kilo == "nee":
+                                print("Sorry, je moet boven de 90 KG zijn.")
+                            else:
+                                kgk = input(certi).lower()
+                                if kgk == "ja":
+                                    print("We nemen jou aan!")
+                                    exit()
+
+
+main()
